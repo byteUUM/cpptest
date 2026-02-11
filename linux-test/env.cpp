bd8517ea6711b9f588e9672929c9a26d27167647
byteUUM <xyk719@126.com>
2026-02-11 22:37:30 +0800

    环境变量测试

--- linux-test/env.cpp
+++ linux-test/env.cpp
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 int main(int argc, char *argv[])
 {
     extern char **environ;
-    for(int i=0; environ[i]; i++) printf("%s\n", environ[i]);
+    //for(int i=0; environ[i]; i++) printf("%s\n", environ[i]);
+    cout<<getenv("PATH")<<endl;
+    cout<<getenv("USER")<<endl;
     return 0;
 }
